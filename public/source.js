986c4829a8aeaa2b04cecfadac36226e33cea943
Preston <greenwood@arizona.edu>
2025-04-26 10:34:29 -0700

    Add files via upload

--- public/source.js
+++ public/source.js
@@ -1,4 +1,21 @@
-
+function insertPromise(doc){
+    client.connect()
+    .then(function(){
+        console.log('connected to Mongodb ...')
+        var db = client.db('testDB2')
+        var coll = db.collection('newCollection')
+        return coll.insertOne(doc)
+    })
+    .then(function(){
+        console.log('Inserted one ...')
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+    .finally(function(){
+        client.close()
+    })
+    
 function storeUser(){
     var username = document.getElementById('username').value
     window.localStorage.setItem('username', username)
@@ -46,7 +63,14 @@ function switchImage(thumbnail){
 }
 
 function addToCart(item){
-    alert("AHHHHHHHHHH!")
+    fetch('/addToCart' , {
+        'headers': {'Content-Type': 'application/json'},
+        'method': 'POST',
+        'body': JSON.stringify({username: item.name, price:item.price})
+    })
+    .then(function (req,res){
+
+    })
 }
 
 function clearUsers(){
@@ -87,15 +111,51 @@ function fetchUserData(){
         return res.json()
     })
     .then(function(userData){
-
         var username = document.getElementById('username')
-        username.innerHTML=JSON.parse(userData).username
+        var address = document.getElementById('address')
+        var email = document.getElementById('email')
+        var name = document.getElementById('name')
+        
+        if (JSON.parse(userData).fullName != undefined){
+            name.innerHTML=JSON.parse(userData).fullName
+        }
+        else {
+            name.innerHTML="none"
+        }
+        username.innerHTML=window.localStorage.getItem('username')
+    
+        if (JSON.parse(userData).email != undefined){
+            email.innerHTML=JSON.parse(userData).email
+        }
+        else {
+            email.innerHTML="none"
+        }
+        if (JSON.parse(userData).address != undefined){
+            address.innerHTML=parseAddress(JSON.parse(userData).address)
+        }
+        else {
+            address.innerHTML="none"
+        }
+        
+        window.localStorage.setItem('firstName',JSON.parse(userData).fullName.split(' ')[0])
+        window.localStorage.setItem('lastName',JSON.parse(userData).fullName.split(' ')[1])
+        window.localStorage.setItem('email', JSON.parse(userData).email)
+        window.localStorage.setItem('address',JSON.parse(userData).address)
     })
     .catch(function (err){
         console.log(err)
     })
 }
 
+function parseAddress(address){
+    var splitAddress = address.split(',')
+    if (splitAddress.length == 5){
+        return `${splitAddress[0]},${splitAddress[4]},${splitAddress[1]},${splitAddress[2]},${splitAddress[3]}`
+    }
+    else {
+        return address
+    }
+}
 function fetchCart(){
     fetch('/api/cart', {
         'headers': {'Content-Type': 'application/json'}
@@ -139,4 +199,36 @@ function changeHref(){
         }  
     }
     
+}
+
+function addFields(){
+    var username = window.localStorage.getItem('username')
+    var spot = document.getElementById('username')
+    spot.value = username
+
+    var firstName = window.localStorage.getItem('firstName')
+    var lastName = window.localStorage.getItem('lastName')
+    var email = window.localStorage.getItem('email')
+    var address = window.localStorage.getItem('address').split(',')
+    var line1Spot = document.getElementById('aLine1')
+    var line2Spot = document.getElementById('aLine2')
+    var citySpot = document.getElementById('aCity')
+    var stateSpot = document.getElementById('aState')
+    var ZIPSpot = document.getElementById('aZIP')
+    line1Spot.placeholder = address[0]
+    citySpot.placeholder = address[1]
+    stateSpot.placeholder = address[2]
+    ZIPSpot.placeholder = address[3]
+
+    if (address.length==5) {
+        line2Spot.placeholder = address[4]
+    }
+
+    var firstSpot = document.getElementById('firstName')
+    var lastSpot = document.getElementById('lastName')
+    var emailSpot = document.getElementById('email')
+
+    firstSpot.placeholder = firstName
+    lastSpot.placeholder = lastName
+    emailSpot.placeholder = email
 }
